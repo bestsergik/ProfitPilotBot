3b4581621a595542fef0f93c2ab8ac2e7b6fe11e
bestsergik <sergynia88@mail.ru>
2023-09-24 08:24:22 +0300

    Change api keys for test binane server and modify file traiding_bot.py

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
-from binance.client import Client
-import ccxt
 import pandas as pd
+from trading_bot import binance as client
 
-# Замените 'your_api_key' и 'your_api_secret' на свои ключи API.
-client = Client(api_key='your_api_key', api_secret='your_api_secret')
-
-# Проверка подключения
-print(client.get_system_status())
+# Проверка подключения (метод fetch_time не является стандартным методом ccxt, но можно использовать его, если биржа поддерживает)
+# Если Binance не поддерживает fetch_time, вы можете просто пропустить этот шаг
+try:
+    timestamp = client.fetch_time()
+    print(f"Время биржи: {timestamp}")
+except Exception as e:
+    print(f"Не удалось получить время биржи: {e}")
 
 def fetch_historical_data(symbol, timeframe):
-    # Инициализация клиента CCXT для получения исторических данных
-    binance = ccxt.binance()
-    # Получение исторических OHLCV (Open/High/Low/Close/Volume) данных для заданного символа и таймфрейма
-    ohlcv = binance.fetch_ohlcv(symbol, timeframe)
+    # Используйте уже импортированный client, а не создавайте новый экземпляр binance
+    ohlcv = client.fetch_ohlcv(symbol, timeframe)
     df = pd.DataFrame(ohlcv, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
     df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
     df.set_index('timestamp', inplace=True)
     return df
+
 
 # Получение исторических данных для BTC/USDT с дневным таймфреймом
 historical_data = fetch_historical_data('BTC/USDT', '1D')
@@ -55,3 +55,68 @@ def create_order(symbol, type, side, amount, price):
 
 # Example: Create a limit buy order for 0.01 BTC at a price of $40,000
 # create_order('BTC/USDT', 'limit', 'buy', 0.01, 40000)
+
+def create_order(symbol, type, side, amount, price, stop_price=None):
+    """
+    Создает ордер на бирже.
+
+    Параметры:
+        symbol (str): Тикер торгуемой пары (например, 'BTC/USDT').
+        type (str): Тип ордера ('LIMIT', 'MARKET', 'STOP_LOSS_LIMIT', и т.д.).
+        side (str): Сторона ордера ('buy' или 'sell').
+        amount (float): Количество актива для покупки/продажи.
+        price (float): Цена, по которой предполагается выполнить ордер.
+        stop_price (float, optional): Цена стоп-лосса. Только для типа 'STOP_LOSS_LIMIT'.
+
+    Возвращает:
+        dict: Информация об ордере.
+    """
+    params = {}
+    if stop_price:
+        params['stopPrice'] = stop_price
+
+    order = binance.create_order(
+        symbol=symbol,
+        type=type,
+        side=side,
+        amount=amount,
+        price=price,
+        params=params
+    )
+    return order
+
+
+# Example: Create a stop-limit sell order
+# create_order('BTC/USDT', 'STOP_LOSS_LIMIT', 'sell', 0.01, 38000, stop_price=39000)
+
+
+# Пример кода для базового бэктестирования
+def backtest(data, capital, strategy):
+    """
+    Производит бэктестирование стратегии на исторических данных.
+
+    Параметры:
+        data (DataFrame): Исторические данные с ценами.
+        capital (float): Начальный капитал.
+        strategy (function): Функция стратегии торговли.
+
+    Возвращает:
+        float: Конечное значение капитала после торговли.
+    """
+    cash = capital
+    position = 0
+    for i in range(len(data)):
+        signal = strategy(data.iloc[:i])
+        price = data['close'].iloc[i]
+        if signal == 'buy' and cash >= price:
+            position += cash // price
+            cash -= position * price
+        elif signal == 'sell' and position > 0:
+            cash += position * price
+            position = 0
+    final_value = cash + position * (data['close'].iloc[-1] if position > 0 else 0)
+    return final_value
+
+
+# Бэктестирование вашего бота
+backtest(historical_data, 10000, generate_signals)
--- trading_bot.py
+++ trading_bot.py
@@ -1,3 +1,4 @@
+# trading_bot.py
 import ccxt
 import os
 from dotenv import load_dotenv
@@ -9,8 +10,18 @@ load_dotenv()
 api_key = os.getenv('BINANCE_API_KEY')
 api_secret = os.getenv('BINANCE_API_SECRET')
 
-# Initialize Binance API client
+# Инициализируйте клиент Binance API с поддержкой Testnet
 binance = ccxt.binance({
     'apiKey': api_key,
-    'secret': api_secret
+    'secret': api_secret,
+    'enableRateLimit': True,  # важно, чтобы избежать блокировки из-за слишком частых запросов
+    'options': {
+        'defaultType': 'future'  # если вы планируете торговать на фьючерсном рынке
+    },
+    'urls': {
+        'api': {
+            'public': 'https://testnet.binance.vision/api/v3',
+            'private': 'https://testnet.binance.vision/api/v3',
+        }
+    }
 })
